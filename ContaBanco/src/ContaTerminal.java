09d0805b1a453ea8f0a73634bf2ee1bb0360e73b
pedroafsnazario <pedro905fernades@gmail.com>
2023-09-06 15:49:58 -0300

    esboço do projeto, "Scanner" será inserido logo.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Bem vindo, por favor insira seu nome");
+        String nomeCliente = "Pedro";
+
+        System.out.println("Por favor, insira sua Agência");
+        String agencia = "122033";
+
+        System.out.println("Por favor, insira seu número");
+        int numero = 1202;
+
+        System.out.println("Por favor, insira seu saldo");
+        double saldo = 2800.89;
+
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+        agencia + " conta " + numero + " e seu saldo de " + saldo + " já está disponível para saque.");
     }
 }
