37603667fbdc23349a3314b2b9d62be596565478
pedroafsnazario <pedro905fernades@gmail.com>
2023-09-07 01:13:59 -0300

    Adição de Scanner

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 
 public class ContaTerminal {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+
         System.out.println("Bem vindo, por favor insira seu nome");
-        String nomeCliente = "Pedro";
+        String nomeCliente = scanner.next();
 
         System.out.println("Por favor, insira sua Agência");
-        String agencia = "122033";
+        String agencia = scanner.next();
 
         System.out.println("Por favor, insira seu número");
-        int numero = 1202;
+        int numero = scanner.nextInt();
 
         System.out.println("Por favor, insira seu saldo");
-        double saldo = 2800.89;
+        double saldo = scanner.nextDouble();
 
 
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +
